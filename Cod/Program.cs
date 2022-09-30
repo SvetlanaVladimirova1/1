8def046d396dd105134691581d0f4cfcbc07f5fb
Svetlana <tina31@rambler.ru>
2022-09-30 13:20:08 +0300

    array

--- Cod/Program.cs
+++ Cod/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine("Первоначальный массив:");
-string[]array = new string[3]{"Hello", ":-)", "7"}; 
-Console.WriteLine(array[0]);
-Console.WriteLine(array[1]);
-Console.WriteLine(array[2]);
- Console.WriteLine("Измененный массив:");                              //объявляем переменную с именем array  и типом string
-for(int i=0; i<array.Length; i++)
-{  
- if (array[i].Length <= 3)                                               // создаем экземпляр массива, используя оператор new.
-Console.WriteLine(array[i]);                                           // выводим массив на печать
-}
+﻿
+Console.WriteLine("Первоначальный массив:");
+string[]array = new string[5]{"Hello", "*", ":-)", "world", "7"}; 
+Console.WriteLine(String.Join("; ", array));
+Console.WriteLine("Измененный массив:");                              
+ for( int i=0; i<array.Length; i++)
+ {  
+  if (array[i].Length <= 3)                                               
+ Console.Write(array[i]+"; ");                                        
+ }
+
